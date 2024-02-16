c3400536b89332cdf7a9bc228434489b3e4b6a92
Niblick1020 <wpsebastian1020@gmail.com>
2024-02-15 23:15:39 -0800

    first push

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// src\app.test.js
 
-test('renders learn react link', () => {
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
