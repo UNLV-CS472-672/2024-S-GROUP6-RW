0e2188e5ce68aaf93f88115f78a59f1730767356
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2024-02-20 18:37:32 -0800

    add MUI

--- src/pages/expenses/ExpensesPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ExpensesPage() {
-  return <h2>Expenses Page</h2>;
-}
-
-export default ExpensesPage;
--- src/pages/my_trips/MyTripsPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function MyTripsPage() {
-  return <h2>My Trips Page</h2>;
-}
-
-export default MyTripsPage;
