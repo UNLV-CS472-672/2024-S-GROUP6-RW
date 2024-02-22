18707b43ed01b32ff3af96d1204eca5ee58dc743
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2024-02-22 00:19:14 -0800

    Push new code that fix few small errors

--- src/pages/getting_started/GettingStartedPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function GettingStartedPage() {
-  return <h2>Getting Started Page</h2>;
-}
-
-export default GettingStartedPage;
--- src/pages/itinerary/ItineraryPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ItineraryPage() {
-  return <h2>Itinerary Page</h2>;
-}
-
-export default ItineraryPage;
