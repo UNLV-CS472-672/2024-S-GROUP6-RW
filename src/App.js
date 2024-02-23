719ab340396945bb513219c662c4a12c24346d0a
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2024-02-22 23:04:02 -0800

    set up base for expense split and fix some small alex codes' errors

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ import React, { useEffect, useState } from "react";
 function App() {
 	// hard code for testing as we not have databse/back end yet
 	const [user, setUser] = useState(null);
-
 	const toggleUser = () => {
 		if (user) {
 			setUser(null); // log out simulation
@@ -37,15 +36,15 @@ function App() {
 			}); // some fake user
 		}
 	};
-	
-	  const updateUser = (newUser) => {
+
+	const updateUser = (newUser) => {
 		setUser(newUser);
-	  };
+	};
 
 	return (
 		// Wrap the app in the ToggleColorMode component so that the theme can be toggled
 		<ToggleColorMode>
-			<AppContent user={user} updateUser={updateUser}/>
+			<AppContent user={user} updateUser={updateUser} />
 			{/*Add some button to test the user by simulate log out*/}
 			<button onClick={toggleUser}>{user ? "Log out" : "Log in"}</button>
 		</ToggleColorMode>
@@ -55,6 +54,7 @@ function App() {
 function AppContent({ user, updateUser }) {
 	// Get the current theme
 	const theme = useTheme();
+	const [openSignIn, setOpenSignIn] = useState(false);
 
 	// Apply the background color to the body element
 	useEffect(() => {
@@ -65,8 +65,15 @@ function AppContent({ user, updateUser }) {
 	return (
 		// Apply the theme to the app
 		<div className="App" style={{ color: theme.palette.text.primary }}>
-			<NavBar user={user} updateUser={updateUser}/>
-			<SignInDialog />
+			<NavBar user={user} updateUser={updateUser} />
+			<SignInDialog
+				open={openSignIn}
+				onClose={() => setOpenSignIn(false)}
+				onSubmit={(details) => {
+					updateUser(details);
+					setOpenSignIn(false);
+				}}
+			/>
 			<Routes>
 				<Route path="/" element={<GettingStartedPage />} />
 				<Route path="/map" element={<MapPage />} />
--- src/components/NavBar/SignInDialog.js
+++ src/components/NavBar/SignInDialog.js
@@ -1,112 +1,113 @@
 import React, { useState } from "react";
 import {
-  Dialog,
-  Button,
-  IconButton,
-  Typography,
-  TextField,
-  Box,
+	Dialog,
+	Button,
+	IconButton,
+	Typography,
+	TextField,
+	Box,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
 const dialogContainerStyle = {
-  backdropFilter: "blur(5px)",
-  "& .MuiPaper-root": {
-    borderRadius: "8px",
-  },
+	backdropFilter: "blur(5px)",
+	"& .MuiPaper-root": {
+		borderRadius: "8px",
+	},
 };
 
 const props = {
-  open: Boolean,
-  onClick: () => {}, // Define your onClick function here
-  onClose: () => {},
-  onSubmit: () => {},
+	open: Boolean,
+	onClick: () => {}, // Define your onClick function here
+	onClose: () => {},
+	onSubmit: () => {},
 };
 
 const SignInDialog = ({ open, onClick, onClose, onSubmit }) => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
+	const handleUsernameChange = (event) => {
+		setUsername(event.target.value);
+	};
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+	const handlePasswordChange = (event) => {
+		setPassword(event.target.value);
+	};
 
-  const handleSubmit = () => {
-    // Call onSubmit with username and password values
-    onSubmit({ username, email, password });
-  };
+	const handleSubmit = () => {
+		// Call onSubmit with username and password values
+		onSubmit({ username, email, password });
+	};
 
-  return (
-    <>
-      <Button
-        sx={{ my: 2, color: "white", display: "block" }}
-        onClick={onClick}
-      >
-        Sign In
-      </Button>
-      <Dialog open={open} onClose={onClose} style={dialogContainerStyle}>
-        <IconButton
-          sx={{ position: "absolute", top: "8px", right: "8px" }}
-          color="inherit"
-          onClick={onClose}
-          aria-label="close"
-        >
-          <CloseIcon />
-        </IconButton>
-        <Box sx={{ width: "500px", height: "350px", p: 2, overflow: "hidden" }}>
-          {" "}
-          {/* Adjust the width and height of the Box */}
-          <Typography variant="h6" gutterBottom align="center">
-            {" "}
-            {/* Center align the text */}
-            Sign In
-          </Typography>
-          <TextField
-            id="outlined-basic-username"
-            label="Username"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-          <TextField
-            id="outlined-basic-email"
-            label="Email"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <TextField
-            id="outlined-basic-password"
-            label="Password"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <Button
-            onClick={handleSubmit}
-            sx={{ mx: "auto", mt: 2, display: "block" }}
-          >
-            Submit
-          </Button>
-        </Box>
-      </Dialog>
-    </>
-  );
+	return (
+		<>
+			<Dialog open={open} onClose={onClose} style={dialogContainerStyle}>
+				<IconButton
+					sx={{ position: "absolute", top: "8px", right: "8px" }}
+					color="inherit"
+					onClick={onClose}
+					aria-label="close"
+				>
+					<CloseIcon />
+				</IconButton>
+				<Box
+					sx={{
+						width: "500px",
+						height: "350px",
+						p: 2,
+						overflow: "hidden",
+					}}
+				>
+					{" "}
+					{/* Adjust the width and height of the Box */}
+					<Typography variant="h6" gutterBottom align="center">
+						{" "}
+						{/* Center align the text */}
+						Sign In
+					</Typography>
+					<TextField
+						id="outlined-basic-username"
+						label="Username"
+						variant="outlined"
+						fullWidth
+						margin="normal"
+						value={username}
+						onChange={handleUsernameChange}
+					/>
+					<TextField
+						id="outlined-basic-email"
+						label="Email"
+						variant="outlined"
+						fullWidth
+						margin="normal"
+						value={email}
+						onChange={handleEmailChange}
+					/>
+					<TextField
+						id="outlined-basic-password"
+						label="Password"
+						variant="outlined"
+						fullWidth
+						margin="normal"
+						value={password}
+						onChange={handlePasswordChange}
+					/>
+					<Button
+						onClick={handleSubmit}
+						sx={{ mx: "auto", mt: 2, display: "block" }}
+					>
+						Submit
+					</Button>
+				</Box>
+			</Dialog>
+		</>
+	);
 };
 
 export default SignInDialog;
