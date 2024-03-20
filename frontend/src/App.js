e834418368a648dead6cc5e69195137f0e578251
Alexander Lauderdale <91444143+axelauda@users.noreply.github.com>
2024-03-20 09:13:57 -0700

    Added Customization
    Added ability for user to customize banner image, profile image, and image border, which are selectable/uploadable images and svg files stored in the images folder.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import ExpensesPage from "./pages/expenses/ExpensesPage";
 import MyTripsPage from "./pages/my_trips/MyTripsPage";
 import LoginPage from "./pages/login/LoginPage";
 import RegisterPage from "./pages/register/RegisterPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 // import nav components
 import NavBar from "./components/NavBar/NavBar";
@@ -22,69 +23,70 @@ import { useTheme } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 function App() {
-	// hard code for testing as we not have databse/back end yet
-	const [user, setUser] = useState(null);
-	const toggleUser = () => {
-		if (user) {
-			setUser(null); // log out simulation
-		} else {
-			setUser({
-				id: 1,
-				username: "test User 1",
-				email: "testUser1@example.com",
-				password: "********",
-			}); // some fake user
-		}
-	};
+  // hard code for testing as we not have databse/back end yet
+  const [user, setUser] = useState(null);
+  const toggleUser = () => {
+    if (user) {
+      setUser(null); // log out simulation
+    } else {
+      setUser({
+        id: 1,
+        username: "test User 1",
+        email: "testUser1@example.com",
+        password: "********",
+      }); // some fake user
+    }
+  };
 
-	const updateUser = (newUser) => {
-		setUser(newUser);
-	};
+  const updateUser = (newUser) => {
+    setUser(newUser);
+  };
 
-	return (
-		// Wrap the app in the ToggleColorMode component so that the theme can be toggled
-		<ToggleColorMode>
-			<AppContent user={user} updateUser={updateUser} />
-			{/*Add some button to test the user by simulate log out*/}
-			<button onClick={toggleUser}>{user ? "Log out" : "Log in"}</button>
-		</ToggleColorMode>
-	);
+  return (
+    // Wrap the app in the ToggleColorMode component so that the theme can be toggled
+    <ToggleColorMode>
+      <AppContent user={user} updateUser={updateUser} />
+      {/*Add some button to test the user by simulate log out*/}
+      <button onClick={toggleUser}>{user ? "Log out" : "Log in"}</button>
+    </ToggleColorMode>
+  );
 }
 
 function AppContent({ user, updateUser }) {
-	// Get the current theme
-	const theme = useTheme();
-	const [openSignIn, setOpenSignIn] = useState(false);
+  // Get the current theme
+  const theme = useTheme();
+  const [openSignIn, setOpenSignIn] = useState(false);
 
-	// Apply the background color to the body element
-	useEffect(() => {
-		// Apply the background color to the body element
-		document.body.style.backgroundColor = theme.palette.background.default;
-	}, [theme.palette.background.default]);
+  // Apply the background color to the body element
+  useEffect(() => {
+    // Apply the background color to the body element
+    document.body.style.backgroundColor = theme.palette.background.default;
+  }, [theme.palette.background.default]);
 
-	return (
-		// Apply the theme to the app
-		<div className="App" style={{ color: theme.palette.text.primary }}>
-			<NavBar user={user} updateUser={updateUser} />
-			<SignInDialog
-				open={openSignIn}
-				onClose={() => setOpenSignIn(false)}
-				onSubmit={(details) => {
-					updateUser(details);
-					setOpenSignIn(false);
-				}}
-			/>
-			<Routes>
-				<Route path="/" element={<GettingStartedPage />} />
-				<Route path="/map" element={<MapPage />} />
-				<Route path="/itinerary" element={<ItineraryPage />} />
-				<Route path="/expenses" element={<ExpensesPage />} />
-				<Route path="/my-trips" element={<MyTripsPage />} />
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/register" element={<RegisterPage />} />
-			</Routes>
-		</div>
-	);
+  return (
+    // Apply the theme to the app
+    <div className="App" style={{ color: theme.palette.text.primary }}>
+      <NavBar user={user} updateUser={updateUser} />
+      <SignInDialog
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)}
+        onSubmit={(details) => {
+          updateUser(details);
+          setOpenSignIn(false);
+        }}
+      />
+      <Routes>
+        <Route path="/" element={<GettingStartedPage />} />
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/itinerary" element={<ItineraryPage />} />
+        <Route path="/expenses" element={<ExpensesPage />} />
+        <Route path="/my-trips" element={<MyTripsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
