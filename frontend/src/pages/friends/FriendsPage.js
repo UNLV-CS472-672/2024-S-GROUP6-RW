cf4b98b9b474ec30147681ecd806a31ececb3779
FPSwitch <sytech8920@protonmail.com>
2024-03-24 12:41:35 -0700

    Updated Friend page by adding a placeholder basic function for adding and removing friends from a list.

--- frontend/src/pages/friends/FriendsPage.js
+++ frontend/src/pages/friends/FriendsPage.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function FriendsPage() {
-    return <h2>My Friends List</h2>
+
+    const [friends, setFriends] = useState([    //List of friends
+        {id: 1, name: 'Test 1'},
+        {id: 2, name: 'Test 2'},
+        {id: 3, name: 'Test 3'},
+        {id: 4, name: 'Test 4'},
+    ]);
+    const [newFriendName, setNewFriendName] = useState('');  //create const for setting new friend
+
+    const addFriend = () => {     //Handles adding a new friend
+        if(newFriendName.trim() != '')
+        {
+            const newFriend = {id: friends.length+1, name: newFriendName};
+            setFriends([...friends, newFriend]);
+            setNewFriendName('');
+        }
+    }
+
+    const removeFriend = (id) => {     //handles removing a friend
+        const updatedFriends = friends.filter((friend) => friend.id != id);
+        setFriends(updatedFriends);
+    }
+
+    return (
+        <>
+    <h2>My Friends List</h2>
+    <input
+        type="text"
+        value={newFriendName}
+        onChange={(e) => setNewFriendName(e.target.value)}
+        placeholder="Enter friend's name:"
+    />
+    <button onClick={addFriend}>Add Friend</button>
+    <ul>
+        {friends.map((friend) => (
+            <li key={friend.id}>
+                {friend.name}
+                <button onClick={() => removeFriend(friend.id)}>Remove</button>
+            </li>
+        ))}
+    </ul>
+
+        </>
+    )
 }
 
 export default FriendsPage;
