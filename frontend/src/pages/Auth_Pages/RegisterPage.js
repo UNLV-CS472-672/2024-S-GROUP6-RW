b24821e0bcfe97de8197ca407723e0a843c315f7
Niblick1020 <wpsebastian1020@gmail.com>
2024-03-25 16:55:47 -0700

    Page organization
    Because all these files are similar I moved them all under the same file under pages. I also implemented the logout page which a user is taken to once signing out

--- frontend/src/pages/Auth_Pages/RegisterPage.js
+++ frontend/src/pages/Auth_Pages/RegisterPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import SignUpForm from "../../components/login-register/SignUpForm";
 
 function RegisterPage() {
   return (
