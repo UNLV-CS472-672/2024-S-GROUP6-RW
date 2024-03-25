65c9ea6564c3e06b282f2788f57087bd2a5faeda
Sebula <wpsebastian1020@gmail.com>
2024-03-24 21:01:17 -0700

    Using this login page instead of the NAV bar

--- frontend/src/pages/login/LoginPage.js
+++ frontend/src/pages/login/LoginPage.js
@@ -1,7 +1,10 @@
+
 import React from "react";
+import SignInDialog from "../../components/NavBar/SignInDialog";
 
 function LoginPage() {
-  return <h2>Login Page</h2>;
-}
+  return (						
+  <SignInDialog open = {true}/>
+  )};
 
 export default LoginPage;
