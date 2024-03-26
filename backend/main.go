3b33dd374255a03aac0cdd767d4e1e168f5525e4
William Simpson <VyProductions.music@gmail.com>
2024-03-26 00:45:06 -0700

    Uncommented handlers which were not yet implemented.

--- backend/main.go
+++ backend/main.go
@@ -23,8 +23,8 @@ func main() {
 	// User Interface
 	r.POST("/register", handlers.RegisterHandler)
 	r.POST("/signin", handlers.SignInHandler)
-	// r.POST("/get_user", handlers.GetUserHandler)
-	// r.POST("/edit_user", handlers.EditUserHandler)
+	r.POST("/get_user", handlers.GetUserHandler)
+	r.POST("/edit_user", handlers.EditUserHandler)
 	r.POST("/delete_user", handlers.DeleteUserHandler)
 
 	// Friend Interface
@@ -32,7 +32,7 @@ func main() {
 	r.POST("/get_friend_requests", handlers.GetFriendRequestsHandler)
 	r.POST("/get_friends", handlers.GetFriendsHandler)
 	r.POST("/acknowledge_friend_request", handlers.AcknowledgeFriendRequestHandler)
-	// r.POST("/remove_friend", handlers.RemoveFriendHandler)
+	r.POST("/remove_friend", handlers.RemoveFriendHandler)
 
 	// Profile Interface
 	r.POST("/get_profile", handlers.GetProfileHandler)
@@ -43,27 +43,27 @@ func main() {
 	// Trip Interface
 	r.POST("/create_trip", handlers.CreateTripHandler)
 	r.POST("/get_all_trips", handlers.GetAllTripsHandler)
-	// r.POST("/get_trip", handlers.GetTripHandler)
+	r.POST("/get_trip", handlers.GetTripHandler)
 	r.POST("/edit_trip", handlers.EditTripHandler)
 	r.POST("/delete_trip", handlers.DeleteTripHandler)
 
 	// Activity Interface
 	r.POST("/create_activity", handlers.CreateActivityHandler)
-	// r.POST("/get_all_activities", handlers.GetAllActivitiesHandler)
+	r.POST("/get_all_activities", handlers.GetAllActivitiesHandler)
 	r.POST("/get_activity", handlers.GetActivityHandler)
 	r.POST("/edit_activity", handlers.EditActivityHandler)
 	r.POST("/delete_activity", handlers.DeleteActivityHandler)
 
 	// Expense Interface
 	r.POST("/create_expense", handlers.CreateExpenseHandler)
-	// r.POST("/get_all_expenses", handlers.GetAllExpensesHandler)
+	r.POST("/get_all_expenses", handlers.GetAllExpensesHandler)
 	r.POST("/get_expense", handlers.GetExpenseHandler)
 	r.POST("/edit_expense", handlers.EditExpenseHandler)
 	r.POST("/delete_expense", handlers.DeleteExpenseHandler)
 
 	// Invoice Interface
 	r.POST("/create_invoice", handlers.CreateInvoiceHandler)
-	// r.POST("/get_all_invoices", handlers.GetAllInvoicesHandler)
+	r.POST("/get_all_invoices", handlers.GetAllInvoicesHandler)
 	r.POST("/get_invoice", handlers.GetInvoiceHandler)
 	r.POST("/edit_invoice", handlers.EditInvoiceHandler)
 	r.POST("/delete_invoice", handlers.DeleteInvoiceHandler)
