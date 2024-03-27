d681745fb2ce6676d420a7f9db1ea361ced68725
amai83 <amyfeng1082@gmail.com>
2024-03-26 17:43:12 -0700

    Added datepicker component into gatheringinfo page. I moved the functions from ItineraryPage to the date picker component. I also rerouted the start button to the map page.

--- frontend/src/components/ItineraryForm/DatePickerComponent.js
+++ frontend/src/components/ItineraryForm/DatePickerComponent.js
@@ -4,31 +4,73 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
   const ItineraryDatePickerComponent = ({ onSelectStartDate, onSelectEndDate, onDateSelectionComplete }) => {
+  const [itinerary, setItinerary] = useState([]); // Itinerary array state
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
-
+  const [selectedStartDate, setSelectedStartDate] = useState(null); // Selected start date
+  const [selectedEndDate, setSelectedEndDate] = useState(null); // Selected end date
+  const navigate = useNavigate();
+  
   const handleStartDateChange = (date) => {
     setStartDate(date);
-    onSelectStartDate(date);
   };
 
   const handleEndDateChange = (date) => {
     setEndDate(date);
-    onSelectEndDate(date);
   };
 
   //Complete only if start date and end date is selected AND the end date is later or on that date of the start date.
   const isDateSelectionComplete = startDate && endDate && startDate <= endDate; 
 
+  const handleStartDateSelect = (date) => {
+    if (date instanceof Date) {
+      setSelectedStartDate(date);
+    } else {
+      setSelectedStartDate(new Date(date));
+    }
+  };
+
+  // Event handler for selecting end date
+  const handleEndDateSelect = (date) => {
+    if (date instanceof Date) {
+      setSelectedEndDate(date);
+    } else {
+      setSelectedEndDate(new Date(date));
+    }
+  };
 
   const handleComplete = () => {
     if (isDateSelectionComplete) {
-      onDateSelectionComplete();
+      //THIS SHOULD BE WHERE THE DATES GET SENT TO DATABASE
+      navigate('/map');
     }
   }
 
+
+  // Event handler for completing date selection
+  const handleDateSelectionComplete = () => {
+    if (selectedStartDate && selectedEndDate) {
+      const numberOfDays = calculateNumberOfDays(
+        selectedStartDate,
+        selectedEndDate
+      );
+
+      if (numberOfDays < 0) {
+        alert("Please select a valid date range.");
+      }
+    }
+  };
+
+  // Function to calculate the number of days between two dates
+  const calculateNumberOfDays = (startDate, endDate) => {
+    const oneDay = 24 * 60 * 60 * 1000;
+    return Math.round(Math.abs((startDate - endDate) / oneDay)) + 1;
+  };
+
+  
   return (
     <div>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -46,11 +88,13 @@ import { Button } from '@mui/material';
           renderInput={(params) => <TextField {...params} />}
         />
         <Button
-          onClick={() => {handleComplete()}}
+          onClick={handleComplete}
           variant='contained'
           disabled={!isDateSelectionComplete}
         >Start</Button>
       </LocalizationProvider>
+
+ 
     </div>
   );
 };
--- frontend/src/pages/getting_started/GatheringInfoPage.js
+++ frontend/src/pages/getting_started/GatheringInfoPage.js
@@ -1,57 +1,18 @@
 import React, { useState } from "react";
-import GatheringInfo from "../../components/GatheringInfo/GatheringInfo";
-import DatePickerComponent from "../../components/ItineraryForm/DatePickerComponent"; // Importing the DatePickerComponent
-import {calculateNumberOfDays, generateItinerary} from "../itinerary/ItineraryPage";
+import GatheringInfo from "../../components/GatheringInfo/DateSelect";
+import DatePickerComponent from "../../components/ItineraryForm/DatePickerComponent"; 
 import SearchBar from "../../components/GatheringInfo/SearchBar";
 
 function GatheringInfoPage() {
-    /*/const [currentCity] = useState("Your City"); // Current city, set to "Your City"
-    //const [itinerary, setItinerary] = useState([]); // Itinerary array state
-    const [currentStep, setCurrentStep] = useState("datePicker"); // Current step in the itinerary process
-    const [selectedStartDate, setSelectedStartDate] = useState(null); // Selected start date
-    const [selectedEndDate, setSelectedEndDate] = useState(null); // Selected end date
     
-    // Event handler for selecting start date
-    const handleStartDateSelect = (date) => {
-        if (date instanceof Date) {
-            setSelectedStartDate(date);
-        } else {
-            setSelectedStartDate(new Date(date));
-        }
-    };
-    
-      // Event handler for selecting end date
-    const handleEndDateSelect = (date) => {
-      if (date instanceof Date) {
-        setSelectedEndDate(date);
-      } else {
-        setSelectedEndDate(new Date(date));
-      }
-    };
-    
-      // Event handler for completing date selection
-    const handleDateSelectionComplete = () => {
-      if (selectedStartDate && selectedEndDate) {
-        const numberOfDays = calculateNumberOfDays(
-          selectedStartDate,
-          selectedEndDate
-        );
-    
-        if (numberOfDays > 0) {
-          generateItinerary(numberOfDays);
-          setCurrentStep("itinerary");
-        } else {
-          alert("Please select a valid date range.");
-        }
-      }
-    };
-  */
-
 
     return (
     <>
         <h2>Plan your trip</h2>
+        <h3> Where would you like to go? </h3>
         <SearchBar />
+        <h3> Select the dates of your trip </h3>
+        <DatePickerComponent />
     </>
   );
 };
