c2023306dcb3affaf9e338cd6f8d22137e660761
amai83 <amyfeng1082@gmail.com>
2024-03-26 19:47:40 -0700

    deleted semi colon that was showing up in the my-trips page.. deleted package-lock.json again as well..

--- frontend/src/pages/my_trips/MyTripsPage.js
+++ frontend/src/pages/my_trips/MyTripsPage.js
@@ -4,7 +4,7 @@ import ButtonBaseDemo from "../../components/ExpensesTripStuff/TripsList";
 function MyTripsPage() {
   return (
     <>
-      <h2>My Trips Page</h2>;
+      <h2>My Trips Page</h2>
       < ButtonBaseDemo />
     </>
   )
