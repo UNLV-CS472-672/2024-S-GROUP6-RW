bb09c149d4ab6b305767666ed91d74afeb2cc00d
SY <94941799+FPSwitch@users.noreply.github.com>
2024-03-26 20:24:02 -0700

    Fixed one of tab values and added documentation comments on friend page related code.

--- frontend/src/components/FriendsPage/FriendsContainer.js
+++ frontend/src/components/FriendsPage/FriendsContainer.js
@@ -4,17 +4,18 @@ import AddRemoveFriends from "../../components/FriendsPage/AddRemoveFriends";
 import FriendList from "../../components/FriendsPage/BoxFriend";
 
 
-//Contains the box for tabs
+//Contains the box for tabs that the user can click on
 function FriendsContainer() {
 	const [value, setValue] = React.useState(0);
-
+	//Handles when you select a tab for the value to change and switch to that content
 	const handleChange = (event, newValue) => {
 		setValue(newValue);
 	};
 
 	return (
-		<Box sx={{ width: "100%", bgcolor: "background.paper" }}>
-			<Tabs
+		<Box //Sets Background color of box area
+			sx={{ width: "100%", bgcolor: "background.paper" }}>
+			<Tabs //Sets the names and style for each tab name
 				value={value}
 				onChange={handleChange}
 				textColor="secondary"
@@ -24,8 +25,10 @@ function FriendsContainer() {
                 <Tab label="Add/Remove Friend" />
 				<Tab label="Friend List" />
 			</Tabs>
-			{value === 0 && <AddRemoveFriends />}
-			{value === 0 && <FriendList />}
+			{value  //Links each tab to a component file 
+				=== 0 && <AddRemoveFriends />}
+			{value
+				=== 1 && <FriendList />}
 		</Box>
 	);
 }
