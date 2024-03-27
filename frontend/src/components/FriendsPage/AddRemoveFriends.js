1af25c37a5f789fb6af148010f46956c0ef8d258
SY <94941799+FPSwitch@users.noreply.github.com>
2024-03-26 20:28:43 -0700

    Revert "Fixed one of tab values and added documentation comments on friend page related code."
    This reverts commit bb09c149d4ab6b305767666ed91d74afeb2cc00d.

--- frontend/src/components/FriendsPage/AddRemoveFriends.js
+++ frontend/src/components/FriendsPage/AddRemoveFriends.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from "react";
 import { Tabs, Tab, Box } from "@mui/material";
 
 function AddRemoveFriends() {
-    // a test to contain when setting list of friends to store temporarily
+    
     const [friends, setFriends] = React.useState([    //List of friends
         //{id: 1, name: 'Test 1'},
         //list of friends would be added here
@@ -12,7 +12,7 @@ function AddRemoveFriends() {
     const addFriend = () => {     //Handles adding a new friend
         if(newFriendName.trim() != '')
         {
-            const newFriend = {id: friends.length+1, name: newFriendName};  //If name does not exist in friend list, then add id and name
+            const newFriend = {id: friends.length+1, name: newFriendName};
             setFriends([...friends, newFriend]);
             setNewFriendName('');
         }
@@ -26,21 +26,18 @@ function AddRemoveFriends() {
     //Showing a textbox to add a person as a friend which shows in a list. You can also remove someone with a button next to the person's name
     return (
         <>
-        <input  //shows a textbox for user to enetr a person's name
+        <input
             type="text"
             value={newFriendName}
             onChange={(e) => setNewFriendName(e.target.value)}
             placeholder="Enter friend's name:"
         />
-        <button  //Button that will add the friend name to a list 
-            onClick={addFriend}>Add Friend</button>
+        <button onClick={addFriend}>Add Friend</button>
         <ul>
             {friends.map((friend) => (
-                <li //Lists out all names in the friends list
-                    key={friend.id}>
-                        {friend.name}
-                        <button   //button to click to remover a friend's name off of the list
-                            onClick={() => removeFriend(friend.id)}>Remove</button>
+                <li key={friend.id}>
+                    {friend.name}
+                    <button onClick={() => removeFriend(friend.id)}>Remove</button>
                 </li>
             ))}
         </ul>
