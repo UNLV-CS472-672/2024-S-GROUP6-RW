e4369446820a6cb83bcba66c7ce94eb59b0d5d77
SY <94941799+FPSwitch@users.noreply.github.com>
2024-03-26 20:29:46 -0700

    revert friends page comments

--- frontend/src/pages/friends/FriendsPage.js
+++ frontend/src/pages/friends/FriendsPage.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { Tabs, Tab, Box } from "@mui/material";
 import FriendsContainer from "../../components/FriendsPage/FriendsContainer";
 
-//This page is to showcase any friends the user has.
-//The data shown on the page will come from the FriendsContainer.js 
+
 function FriendsPage() {
     return (
       <>
