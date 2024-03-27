5f337975d854b9496ce9b03e7b9405fb5e479577
SY <94941799+FPSwitch@users.noreply.github.com>
2024-03-26 20:34:37 -0700

    fixed value for one of the tabs and removed packagelock

--- frontend/src/components/FriendsPage/FriendsContainer.js
+++ frontend/src/components/FriendsPage/FriendsContainer.js
@@ -4,17 +4,18 @@ import AddRemoveFriends from "../../components/FriendsPage/AddRemoveFriends";
 import FriendList from "../../components/FriendsPage/BoxFriend";
 
 
-//Contains the box for tabs
+//Contains the box for tabs that the user can click on
 function FriendsContainer() {
 	const [value, setValue] = React.useState(0);
-
+	
 	const handleChange = (event, newValue) => {
 		setValue(newValue);
 	};
 
 	return (
-		<Box sx={{ width: "100%", bgcolor: "background.paper" }}>
-			<Tabs
+		<Box 
+			sx={{ width: "100%", bgcolor: "background.paper" }}>
+			<Tabs 
 				value={value}
 				onChange={handleChange}
 				textColor="secondary"
@@ -24,8 +25,10 @@ function FriendsContainer() {
                 <Tab label="Add/Remove Friend" />
 				<Tab label="Friend List" />
 			</Tabs>
-			{value === 0 && <AddRemoveFriends />}
-			{value === 0 && <FriendList />}
+			{value  
+				=== 0 && <AddRemoveFriends />}
+			{value
+				=== 1 && <FriendList />}
 		</Box>
 	);
 }
