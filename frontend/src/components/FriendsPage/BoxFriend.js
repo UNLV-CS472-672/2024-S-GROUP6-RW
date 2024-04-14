e57f62ca773679b8acccfabb389a78dceeabb413
SY <94941799+FPSwitch@users.noreply.github.com>
2024-04-13 22:03:03 -0700

    Updated CSS and BoxFriend.js so it shows an example of friends in a list. Currently centered.

--- frontend/src/components/FriendsPage/BoxFriend.js
+++ frontend/src/components/FriendsPage/BoxFriend.js
@@ -14,97 +14,44 @@ function BoxFriend() {
                 </div>
                 <div class="body">
                     <ul class="new_friend_list list-unstyled row">
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
+                        <li>
                             <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="img-thumbnail" alt="User Image"></img>
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image" width="100" height="100"></img>
                                 <h6 class="users_name">Jackson</h6>
                                 <small class="join_date">Today</small>
                             </a>
                         </li>
                         <li class="col-lg-4 col-md-2 col-sm-6 col-4">
                             <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-thumbnail" alt="User Image"></img>
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
                                 <h6 class="users_name">Aubrey</h6>
                                 <small class="join_date">Yesterday</small>
                             </a>
                         </li>
                         <li class="col-lg-4 col-md-2 col-sm-6 col-4">
                             <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar5.png" class="img-thumbnail" alt="User Image"></img>
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
                                 <h6 class="users_name">Oliver</h6>
                                 <small class="join_date">08 Nov</small>
                             </a>
                         </li>
                         <li class="col-lg-4 col-md-2 col-sm-6 col-4">
                             <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar3.png" class="img-thumbnail" alt="User Image"></img>
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
                                 <h6 class="users_name">Isabella</h6>
                                 <small class="join_date">12 Dec</small>
                             </a>
                         </li>
                         <li class="col-lg-4 col-md-2 col-sm-6 col-4">
                             <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="img-thumbnail" alt="User Image"></img>
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
                                 <h6 class="users_name">Jacob</h6>
                                 <small class="join_date">12 Dec</small>
                             </a>
                         </li>
                         <li class="col-lg-4 col-md-2 col-sm-6 col-4">
                             <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="img-thumbnail" alt="User Image"></img>
-                                <h6 class="users_name">Matthew</h6>
-                                <small class="join_date">17 Dec</small>
-                            </a>
-                        </li>                            
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-12 col-lg-6">
-            <div class="card">
-                <div class="header">
-                    <h2><strong>New</strong> Friends <small>Add new friend in last month</small></h2>
-                </div>
-                <div class="body">
-                    <ul class="new_friend_list list-unstyled row">
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="img-thumbnail" alt="User Image"></img>
-                                <h6 class="users_name">Jackson</h6>
-                                <small class="join_date">Today</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar5.png" class="img-thumbnail" alt="User Image"></img>
-                                <h6 class="users_name">Aubrey</h6>
-                                <small class="join_date">Yesterday</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="img-thumbnail" alt="User Image"></img>
-                                <h6 class="users_name">Oliver</h6>
-                                <small class="join_date">08 Nov</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar3.png" class="img-thumbnail" alt="User Image"></img>
-                                <h6 class="users_name">Isabella</h6>
-                                <small class="join_date">12 Dec</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-thumbnail" alt="User Image"></img>
-                                <h6 class="users_name">Jacob</h6>
-                                <small class="join_date">12 Dec</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="img-thumbnail" alt="User Image"></img>
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
                                 <h6 class="users_name">Matthew</h6>
                                 <small class="join_date">17 Dec</small>
                             </a>
