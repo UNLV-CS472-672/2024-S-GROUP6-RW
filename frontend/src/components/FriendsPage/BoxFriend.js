304cabb2a6b7a1f30b37d184425878b0c7729a7b
SY <94941799+FPSwitch@users.noreply.github.com>
2024-04-16 15:50:05 -0700

    Made the list of friends into a grid format.

--- frontend/src/components/FriendsPage/BoxFriend.js
+++ frontend/src/components/FriendsPage/BoxFriend.js
@@ -5,64 +5,30 @@ import "../../css/FriendsPage.css";
 function BoxFriend() {
 
     return (
-        <div class="container">
-    <div class="row">
-        <div class="col-md-12 col-lg-6">
-            <div class="card">
-                <div class="header">
-                    <h2><strong>Current</strong> Friends </h2>
-                </div>
-                <div class="body">
-                    <ul class="new_friend_list list-unstyled row">
-                        <li>
-                            <a href="">
-                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image" width="100" height="100"></img>
-                                <h6 class="users_name">Jackson</h6>
-                                <small class="join_date">Added Today</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
-                                <h6 class="users_name">Aubrey</h6>
-                                <small class="join_date">Added Yesterday</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
-                                <h6 class="users_name">Oliver</h6>
-                                <small class="join_date">Added 08 Nov</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
-                                <h6 class="users_name">Isabella</h6>
-                                <small class="join_date">Added 12 Dec</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
-                                <h6 class="users_name">Jacob</h6>
-                                <small class="join_date">Added 12 Dec</small>
-                            </a>
-                        </li>
-                        <li class="col-lg-4 col-md-2 col-sm-6 col-4">
-                            <a href="">
-                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="avatar" alt="User Image"></img>
-                                <h6 class="users_name">Matthew</h6>
-                                <small class="join_date">Added 17 Dec</small>
-                            </a>
-                        </li>                            
-                    </ul>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 col-lg-6">
+                    <div className="card">
+                        <div className="header">
+                            <h2><strong>Current</strong> Friends</h2>
+                        </div>
+                        <div className="body">
+                            <ul className="new_friend_list list-unstyled">
+                                {['Jackson', 'Aubrey', 'Oliver', 'Isabella', 'Jacob', 'Matthew', 'Austin', 'Tyler'].map((name, index) => (
+                                    <li key={index} className="friend-item">
+                                        <a href="">
+                                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" className="avatar" alt="User Image" />
+                                            <h6 className="users_name">{name}</h6>
+                                            <small className="join_date">Added {index % 2 === 0 ? 'Today' : 'Yesterday'}</small>
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-        
     );
 }
 
