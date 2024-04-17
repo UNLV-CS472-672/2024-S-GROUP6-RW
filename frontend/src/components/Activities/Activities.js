c1981fb52389de9fa8aef9f17f69d444b7f96c6f
Ramirez-Christopher <chris.ramirez@nevada.unr.edu>
2024-04-17 16:58:42 -0700

    added location to every destination

--- frontend/src/components/Activities/Activities.js
+++ frontend/src/components/Activities/Activities.js
@@ -9,93 +9,107 @@ const locations = [
     lat: 36.1173, 
     lng: -115.1761, //las vegas
     popularLocations: [
-      { name: 'Las Vegas Welcome Sign',description:"The Welcome to Fabulous Las Vegas sign is a Las Vegas landmark.", rating: 5, image: 'https://media-cdn.tripadvisor.com/media/photo-m/1280/2a/34/2d/28/caption.jpg' },
-      { name: 'MGM Park',description: "Park MGM Las Vegas is in the heart of all there is to do on the Las Vegas Strip. ",rating: 3, image: 'https://ik.imgkit.net/3vlqs5axxjf/external/ik-seo/https://www.cfmedia.vfmleonardo.com/imageRepo/7/0/143/431/660/Park_MGM_-_Hero_2_Cropped_O/Park-MGM-Exterior.jpg?tr=w-780%2Ch-437%2Cfo-auto  ' },
-      { name: 'Sphere', description:"Sphere is a music and entertainment arena",rating: 4, image: 'https://www.rollingstone.com/wp-content/uploads/2023/06/MSG-Sphere1_credit-MSG-Sphere.jpg?w=1581&h=1028&crop=1' }
+      { name: 'Las Vegas Welcome Sign', description: "The Welcome to Fabulous Las Vegas sign is a Las Vegas landmark.", rating: 5, image: 'https://media-cdn.tripadvisor.com/media/photo-m/1280/2a/34/2d/28/caption.jpg', price: '$' },
+      { name: 'MGM Park', description: "Park MGM Las Vegas is in the heart of all there is to do on the Las Vegas Strip.", rating: 3, image: 'https://ik.imgkit.net/3vlqs5axxjf/external/ik-seo/https://www.cfmedia.vfmleonardo.com/imageRepo/7/0/143/431/660/Park_MGM_-_Hero_2_Cropped_O/Park-MGM-Exterior.jpg?tr=w-780%2Ch-437%2Cfo-auto', price: '$$' },
+      { name: 'Sphere', description: "Sphere is a music and entertainment arena", rating: 4, image: 'https://www.rollingstone.com/wp-content/uploads/2023/06/MSG-Sphere1_credit-MSG-Sphere.jpg?w=1581&h=1028&crop=1', price: '$$$' }
     ]
   },
   { 
     lat: 51.1657, 
     lng: 10.4515, 
     popularLocations: [
-      { name: 'Germany', image: 'https://www.deutschland.de/sites/default/files/styles/image_carousel_mobile/public/media/image/tdt_29092020_1_staat_16_laender_schwerin.jpg?itok=4PP80WP4' }
+      { name: 'Germany', description: "Germany is a country in Central Europe.", image: 'https://www.deutschland.de/sites/default/files/styles/image_carousel_mobile/public/media/image/tdt_29092020_1_staat_16_laender_schwerin.jpg?itok=4PP80WP4', price: '$$' }
     ]
   },
   { 
     lat: 46.2276, 
     lng: 2.2137, 
     popularLocations: [
-      { name: 'France', image: 'https://www.travelandleisure.com/thmb/9xr8CFGR14sLvR4IhLwKV64fEV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-Eiffel-Tower-BESTFRANCE0323-dada0673f8764c099b68d01695ef8057.jpg' }
+      { name: 'France', description: "France is a country located in Western Europe.", image: 'https://www.travelandleisure.com/thmb/9xr8CFGR14sLvR4IhLwKV64fEV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-Eiffel-Tower-BESTFRANCE0323-dada0673f8764c099b68d01695ef8057.jpg', price: '$$$' }
     ]
   },
   { 
     lat: 19.8987, 
     lng: 155.6659, 
     popularLocations: [
-      { name: 'Hawaii', image: 'https://www.travelandleisure.com/thmb/9xr8CFGR14sLvR4IhLwKV64fEV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-Eiffel-Tower-BESTFRANCE0323-dada0673f8764c099b68d01695ef8057.jpg' }
+      { name: 'Hawaii', description: "Hawaii is a U.S. state located in the Pacific Ocean.", image: 'https://www.travelandleisure.com/thmb/9xr8CFGR14sLvR4IhLwKV64fEV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-Eiffel-Tower-BESTFRANCE0323-dada0673f8764c099b68d01695ef8057.jpg', price: '$$$' }
     ]
   },
   { 
     lat: 41.8967, 
     lng: 12.4822, 
     popularLocations: [
-      { name: 'Rome', image: 'https://www.travelandleisure.com/thmb/9xr8CFGR14sLvR4IhLwKV64fEV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-Eiffel-Tower-BESTFRANCE0323-dada0673f8764c099b68d01695ef8057.jpg' }
+      { name: 'Rome', description: "Rome is the capital city of Italy.", image: 'https://www.travelandleisure.com/thmb/9xr8CFGR14sLvR4IhLwKV64fEV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-Eiffel-Tower-BESTFRANCE0323-dada0673f8764c099b68d01695ef8057.jpg', price: '$$$' }
     ]
   },
   { 
     lat: 35.6764, 
     lng: 139.6500, 
     popularLocations: [
-      { name: 'Tokyo', image: 'https://hips.hearstapps.com/hmg-prod/images/high-angle-view-of-tokyo-skyline-at-dusk-japan-royalty-free-image-1664309926.jpg' }
+      { name: 'Tokyo', description: "Tokyo is the capital city of Japan.", image: 'https://hips.hearstapps.com/hmg-prod/images/high-angle-view-of-tokyo-skyline-at-dusk-japan-royalty-free-image-1664309926.jpg', price: '$$' }
     ]
   },
   { 
     lat: 20.7984, 
     lng: 156.3319, 
     popularLocations: [
-      { name: 'Maui', image: 'https://a.cdn-hotels.com/gdcs/production81/d1269/0cffe15a-7fdf-4e75-9415-92eaf78e2f73.jpg' }
+      { name: 'Maui', description: "Maui is an island in Hawaii.", image: 'https://a.cdn-hotels.com/gdcs/production81/d1269/0cffe15a-7fdf-4e75-9415-92eaf78e2f73.jpg', price: '$$$' }
     ]
   },
   { 
     lat: 25.0343, 
     lng: 77.3963, 
     popularLocations: [
-      { name: 'Bahamas', image: 'https://thepointsguy.global.ssl.fastly.net/us/originals/2022/03/HI_B_KSchaler.jpg' }
+      { name: 'Bahamas', description: "The Bahamas is a country in the Caribbean.", image: 'https://thepointsguy.global.ssl.fastly.net/us/originals/2022/03/HI_B_KSchaler.jpg', price: '$$$' }
     ]
   },
   { 
     lat: 51.5072, 
     lng: 0.1276, 
     popularLocations: [
-      { name: 'London', image: 'https://a.cdn-hotels.com/gdcs/production153/d1371/e6c1f55e-51ac-41d5-8c63-2d0c63faf59e.jpg' }
+      { name: 'London', description: "London is the capital city of England.", image: 'https://a.cdn-hotels.com/gdcs/production153/d1371/e6c1f55e-51ac-41d5-8c63-2d0c63faf59e.jpg', price: '$$$' }
     ]
   },
   { 
     lat: 16.5004, 
     lng: 151.7415, 
     popularLocations: [
-      { name: 'Bora Bora', image: 'https://cache.marriott.com/content/dam/marriott-renditions/BOBXR/bobxr-exterior-aerialview-1580-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=2880px:*' }
+      { name: 'Bora Bora', description: "Bora Bora is an island in French Polynesia.", image: 'https://cache.marriott.com/content/dam/marriott-renditions/BOBXR/bobxr-exterior-aerialview-1580-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=2880px:*', price: '$$$$' }
     ]
   },
   { 
     lat: 17.6509, 
     lng: 149.4260, 
     popularLocations: [
-      { name: 'Tahiti', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE6IkkwdqD57Dvlbe3JEwPtwEMNxX_0UKfeIIjADevwA&s' }
+      { name: 'Tahiti', description: "Tahiti is the largest island in French Polynesia.", image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE6IkkwdqD57Dvlbe3JEwPtwEMNxX_0UKfeIIjADevwA&s', price: '$$$$' }
     ]
   },
   { 
     lat: 40.7128, 
     lng: 74.0060, 
     popularLocations: [
-      { name: 'New York', image: 'https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg' }
+      { name: 'New York', description: "New York City is the most populous city in the United States.", image: 'https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg', price: '$$$' }
     ]
   },
   { 
     lat: 8.4095, 
     lng: 115.1889, 
     popularLocations: [
-      { name: 'Bali', image: 'https://media.cntraveler.com/photos/63f4f4fc0630e21ed8088321/1:1/w_1280%2Ch_1280%2Cc_limit/GettyImages-1145042281.jpeg' }
+      { name: 'Bali', description: "Bali is an island and province of Indonesia.", image: 'https://media.cntraveler.com/photos/63f4f4fc0630e21ed8088321/1:1/w_1280%2Ch_1280%2Cc_limit/GettyImages-1145042281.jpeg', price: '$$$' }
+    ]
+  },
+  {
+    lat: 43.7700, 
+    lng: 11.2577,
+    popularLocations:[
+      { name: "Florence", description: "Florence is the capital city of the Italian region of Tuscany.", image: "https://content.r9cdn.net/rimg/dimg/ee/c3/840e901d-city-25345-158f32d4e66.jpg?width=1366&height=768&xhint=1940&yhint=1356&crop=true", price:'$$$' }
+    ]
+  },
+  {
+    lat: 13.1632, 
+    lng: 72.5453, 
+    popularLocations: [
+      {name: "Machu Picchu", description: "Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru.", image: "https://travel.home.sndimg.com/content/dam/images/travel/fullset/2014/05/28/50/Machu-Picchu-Mountain.rend.hgtvcom.1280.1280.suffix/1491584261034.jpeg",price: '$$$' }
     ]
   }
 ]
@@ -140,6 +154,7 @@ const ActComponent = ({ lat, lng }) => {
                   name='rating'
                   starDimension="20px" // Set the size of the stars
                 />
+                <p>{popularLocation.price}</p>
               </li>
             ))}
           </ul>
@@ -150,6 +165,13 @@ const ActComponent = ({ lat, lng }) => {
           <button className="close-button" onClick={handleClose}>X</button>
           <img src={popupLocation.image} alt={popupLocation.name} />
           <h2>{popupLocation.name}</h2>
+          <StarRatings
+            rating={popupLocation.rating}
+            starRatedColor="gold"
+            numberOfStars={5}
+            name='rating'
+            starDimension="20px"
+          />
           <p>{popupLocation.description}</p>
           <button onClick={handleAddToList}>Add to List</button>
         </div>
