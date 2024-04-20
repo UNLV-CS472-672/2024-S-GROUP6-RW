813d57d6050f554836f5ea4f5e65fd08f9e6d961
renm10 <43124584+renm10@users.noreply.github.com>
2024-04-19 21:29:40 -0700

    Chagned calendar UI and edit button

--- frontend/src/components/ItineraryForm/EditView.js
+++ frontend/src/components/ItineraryForm/EditView.js
@@ -8,8 +8,9 @@ import "../../css/ReactBigCalendar.css";
 import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
 import CloseIcon from "@mui/icons-material/Close";
-import DeleteIcon from '@mui/icons-material/Delete';
-import NotesSharpIcon from '@mui/icons-material/NotesSharp';
+import DeleteIcon from "@mui/icons-material/Delete";
+import NotesSharpIcon from "@mui/icons-material/NotesSharp";
+
 import {
   Dialog,
   DialogTitle,
@@ -32,7 +33,12 @@ import { deepOrange, orange, red } from "@mui/material/colors";
 
 const localizer = momentLocalizer(moment);
 
-const EditView = ({ day, userActivities, onUpdatedActivities, onClickCloseButton }) => {
+const EditView = ({
+  day,
+  userActivities,
+  onUpdatedActivities,
+  onClickCloseButton,
+}) => {
   const dayObj = new Date(day); //Convert day string to date object
 
   const [showEditDialog, setShowEditDialog] = useState(false);
@@ -67,7 +73,7 @@ const EditView = ({ day, userActivities, onUpdatedActivities, onClickCloseButton
     setCreateTitle("");
     setCreateLocation("");
     setCreateDescription("");
-  }
+  };
 
   const handleSave = () => {
     if (selectedActivity) {
@@ -83,7 +89,9 @@ const EditView = ({ day, userActivities, onUpdatedActivities, onClickCloseButton
           end: endTime ? new Date(endTime) : selectedActivity.end,
           title: title || selectedActivity.title,
         };
-        updatedEvents.sort((actObjx, actObjy) => {return actObjx.start - actObjy.start}); //Sort them depending on the activites start time
+        updatedEvents.sort((actObjx, actObjy) => {
+          return actObjx.start - actObjy.start;
+        }); //Sort them depending on the activites start time
         onUpdatedActivities(updatedEvents); //Pass the updated activity back to the parent
       }
       setShowEditDialog(false);
@@ -100,7 +108,7 @@ const EditView = ({ day, userActivities, onUpdatedActivities, onClickCloseButton
       setShowEditDialog(false);
       setSelectedActivity(null);
     }
-  }
+  };
 
   const handleCreateEvent = () => {
     if (createStartTime && createEndTime) {
@@ -113,7 +121,9 @@ const EditView = ({ day, userActivities, onUpdatedActivities, onClickCloseButton
         location: createLocation || "",
       };
       const updatedActivity = [...userActivities, newEvent];
-      updatedActivity.sort((actObjx, actObjy) => {return actObjx.start - actObjy.start}); //Sort them depending on the activites start time
+      updatedActivity.sort((actObjx, actObjy) => {
+        return actObjx.start - actObjy.start;
+      }); //Sort them depending on the activites start time
       onUpdatedActivities(updatedActivity); //Pass to the parent component.
       setShowCreateEventDialog(false); //Close the create event dialog
 
@@ -126,154 +136,185 @@ const EditView = ({ day, userActivities, onUpdatedActivities, onClickCloseButton
     } else {
       /* Required Fields are missing case */
     }
-  }
+  };
 
   return (
     <>
-      <div className="big-calendar">
-        {" "}
+      <br></br>
+      <br></br>
+      <div className="button-section">
+        <IconButton className="calendar-close-btn" onClick={onClickCloseButton}>
+          <CloseIcon />
+        </IconButton>
+      </div>
+      <div className="calendar-div">
         {/* Adjust height as needed */}
         <Calendar
+          className="big-calendar"
           events={userActivities}
           defaultView={Views.DAY} // Display day view by default
-          views={[Views.DAY]} // Only show day view
+          views={[Views.DAY, Views.WEEK, Views.MONTH]} // Only show day view
           selectable={true} // Allow selecting time slots
           step={15} // Time slot interval in minutes
           timeslots={4} // Number of time slots per hour
           defaultDate={dayObj} // Display chosen day of the itinerary
           localizer={localizer}
+          onSelectEvent={handleEditButtonClick}
           components={{
-            event: (props) => (
-              <CustomEventComponent
-                {...props}
-                onEditButtonClick={handleEditButtonClick}
-              />
-            ),
+            event: CustomEventComponent
           }}
         />
-        <Dialog
-          className="edit-dialog"
-          open={showEditDialog}
-          onClose={handleEditDialogClose}
-          maxWidth="sm"
-          fullWidth
-        >
-          <DialogTitle>Edit Event</DialogTitle>
-          <DialogContent className="edit-dialog-content">
-            <div className="edit-delete-btn" style={{position: 'absolute', top: 12, right: 5,}}>
-              <IconButton 
-                className="delete-event-btn" 
-                onClick={handleDelete} 
-                sx={{
-                  color: 'transparent',
-                  "&:hover": {
-                    color: red[700],
-                    cursor: "pointer", 
-                  }
-                }}
-                >
-                <DeleteIcon />
-              </IconButton>
-            </div>
-            {selectedActivity && (
-              <div>
-                <FormControl variant="standard">
-                  <InputLabel>Title</InputLabel>
-                  <Input label="Enter Title" defaultValue={selectedActivity.title}  onChange={(e) => setTitle(e.target.value)}/>
-                </FormControl>
-                {/* Other event details */}
-                <div className="start-end-timepicker">
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DateTimePicker
-                      label="Start Time"
-                      value={startTime}
-                      onChange={(newStartTime) => setStartTime(newStartTime)}
-                    />
-                    <DateTimePicker
-                      label="End Time"
-                      value={endTime}
-                      onChange={(newEndTime) => setEndTime(newEndTime)}
-                    />
-                  </LocalizationProvider>
-                </div>
-              </div>
-            )}
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleEditDialogClose}>Cancel</Button>
-            <Button onClick={handleSave}>Save</Button>
-          </DialogActions>
-        </Dialog>
-        <Dialog
-          className="create-event-dialog"
-          open={showCreateEventDialog}
-          onClose={handleCreateEventDialogClose}
-          maxWidth="sm"
-          fullWidth
-        >
-          <DialogTitle>Create Event</DialogTitle>
-          <DialogContent className="create-event-dialog-content">
-            <div className="activity-title-div">
-              <FormControl className="activity-title-formcontrol" variant="standard">
-                <TextField className="activity-title-textfield" label="Title" variant="standard" value={createTitle} onChange={(e) => setCreateTitle(e.target.value)}></TextField>
-              </FormControl>
-            </div>
-            <div className="start-end-timepicker">
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateTimePicker
-                  label="Start Time"
-                  value={createStartTime}
-                  onChange={(newStartTime) => setCreateStartTime(newStartTime)}
-                />
-                <DateTimePicker
-                  label="End Time"
-                  value={createEndTime}
-                  onChange={(newEndTime) => setCreateEndTime(newEndTime)}
-                />
-              </LocalizationProvider>
-            </div>
-            <div className="address-input">
-              <InputBase placeholder="Search Address" value={createLocation} onChange={(e) => setCreateLocation(e.target.value)} />
-            </div>
-            <div className="description-input">
-              <NotesSharpIcon style={{ marginBottom: -6 , paddingRight: 5 }}/>
-              <InputBase className="description-inputbase" placeholder="Description" value={createDescription} onChange={(e) => setCreateDescription(e.target.value)} multiline/>
-            </div>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCreateEventDialogClose}>Cancel</Button>
-            <Button onClick={handleCreateEvent}>Save</Button>
-          </DialogActions>
-        </Dialog>
-        <IconButton className="calendar-close-btn" onClick={onClickCloseButton}>
-          <CloseIcon />
-        </IconButton>
-        <Button variant="outlined" endIcon={<AddIcon />} onClick={() => setShowCreateEventDialog(true)}> Create </Button>
       </div>
+      <Dialog
+        className="edit-dialog"
+        open={showEditDialog}
+        onClose={handleEditDialogClose}
+        maxWidth="sm"
+        fullWidth
+      >
+        <DialogTitle>Edit Event</DialogTitle>
+        <DialogContent className="edit-dialog-content">
+          <div
+            className="edit-delete-btn"
+            style={{ position: "absolute", top: 12, right: 5 }}
+          >
+            <IconButton
+              className="delete-event-btn"
+              onClick={handleDelete}
+              sx={{
+                color: "transparent",
+                "&:hover": {
+                  color: red[700],
+                  cursor: "pointer",
+                },
+              }}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </div>
+          {selectedActivity && (
+            <div>
+              <FormControl variant="standard">
+                <InputLabel>Title</InputLabel>
+                <Input
+                  label="Enter Title"
+                  defaultValue={selectedActivity.title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </FormControl>
+              {/* Other event details */}
+              <div className="start-end-timepicker">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DateTimePicker
+                    label="Start Time"
+                    value={startTime}
+                    onChange={(newStartTime) => setStartTime(newStartTime)}
+                  />
+                  <DateTimePicker
+                    label="End Time"
+                    value={endTime}
+                    onChange={(newEndTime) => setEndTime(newEndTime)}
+                  />
+                </LocalizationProvider>
+              </div>
+            </div>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleEditDialogClose}>Cancel</Button>
+          <Button onClick={handleSave}>Save</Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        className="create-event-dialog"
+        open={showCreateEventDialog}
+        onClose={handleCreateEventDialogClose}
+        maxWidth="sm"
+        fullWidth
+      >
+        <DialogTitle>Create Event</DialogTitle>
+        <DialogContent className="create-event-dialog-content">
+          <div className="activity-titleform-div">
+            <FormControl
+              className="activity-title-formcontrol"
+              variant="standard"
+            >
+              <TextField
+                className="activity-title-textfield"
+                label="Title"
+                variant="standard"
+                value={createTitle}
+                onChange={(e) => setCreateTitle(e.target.value)}
+              ></TextField>
+            </FormControl>
+          </div>
+          <div className="start-end-timepicker">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateTimePicker
+                label="Start Time"
+                value={createStartTime}
+                onChange={(newStartTime) => setCreateStartTime(newStartTime)}
+              />
+              <DateTimePicker
+                label="End Time"
+                value={createEndTime}
+                onChange={(newEndTime) => setCreateEndTime(newEndTime)}
+              />
+            </LocalizationProvider>
+          </div>
+          <div className="address-input">
+            <InputBase
+              placeholder="Search Address"
+              value={createLocation}
+              onChange={(e) => setCreateLocation(e.target.value)}
+            />
+          </div>
+          <div className="description-input">
+            <NotesSharpIcon style={{ marginBottom: -6, paddingRight: 5 }} />
+            <InputBase
+              className="description-inputbase"
+              placeholder="Description"
+              value={createDescription}
+              onChange={(e) => setCreateDescription(e.target.value)}
+              multiline
+            />
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCreateEventDialogClose}>Cancel</Button>
+          <Button onClick={handleCreateEvent}>Save</Button>
+        </DialogActions>
+      </Dialog>
+      <Button
+        className="create-event-btn"
+        variant="outlined"
+        endIcon={<AddIcon />}
+        onClick={() => setShowCreateEventDialog(true)}
+        sx={{
+          marginTop: 2,
+        }}
+      >
+        Create
+      </Button>
     </>
   );
 };
 
-const CustomEventComponent = ({ event, onEditButtonClick }) => {
-  const handleEditClick = () => {
-    onEditButtonClick(event);
-  };
-
+const CustomEventComponent = (event) => {
   return (
-    <div className="activity-title-editbtn">
-      <Typography className="activity-title-label">{event.title}</Typography>
-      <Button
-        className="activity-edit-btn"
-        variant="contained"
-        onClick={handleEditClick}
+    <div className="activity-title-div">
+      <Typography
         sx={{
-          bgcolor: deepOrange[500],
+          fontSize: "15px",
+          height: "100%",
+          position: "relative",
+          top: "-3px",
+          left: "2px",
         }}
       >
-        Edit
-      </Button>
+        {event.title}
+      </Typography>
     </div>
   );
 };
-
 export default EditView;
