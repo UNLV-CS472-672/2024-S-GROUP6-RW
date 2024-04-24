3e3ee310784c7681cfc2110268e0f3e6a01ddb6e
VyProductions <VyProductions.music@gmail.com>
2024-04-24 04:05:04 -0700

    Updated .env semantics to load the main .env file for the backend from major backend directories instead of searching in the current directory.

--- backend/secrets/env.go
+++ backend/secrets/env.go
@@ -1,16 +1,65 @@
 package secrets
 
 import (
-	"github.com/joho/godotenv"
 	"errors"
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
 )
 
-func GetEnv(key string) (string, error) {
-	err := godotenv.Load()
-	if err != nil {
-		return "", errors.New(".env file does not exist")
+var envData map[string]string
+
+func LoadEnv() error {
+	dir, err := os.Getwd()
+
+	pathSplit := strings.Split(dir, "/")
+
+	switch pathSplit[len(pathSplit)-1] {
+	case "2024-S-GROUP6-RW":
+		dir += "/backend"
+	case "backend":
+		// .env file is here
+	case "business":
+		dir += "/.."
+	case "unit_tests":
+		dir += "/../.."
+	default:
+		return errors.New("invalid execution directory")
 	}
 
-	return string(os.Getenv(key)), nil
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(dir)
+
+	err = godotenv.Load(dir + "/.env")
+
+	if err != nil {
+		return errors.New(".env file does not exist")
+	}
+
+	envData = map[string]string{}
+
+	for _, pair := range os.Environ() {
+		parts := strings.Split(pair, "=")
+		key := parts[0]
+		value := parts[1]
+
+		envData[key] = value
+	}
+
+	return nil
+}
+
+func GetEnv(key string) (string, error) {
+	value, ok := envData[key]
+
+	if !ok {
+		return "", errors.New("failed to locate key in .env file")
+	}
+
+	return value, nil
 }
