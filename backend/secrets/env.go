ff28cc2d65baf23311d57dce5a64e286488ed680
VyProductions <VyProductions.music@gmail.com>
2024-04-24 14:14:12 -0700

    Fixed OS-specific .env path issue due to unique file path separator characters on different operating systems.

--- backend/secrets/env.go
+++ backend/secrets/env.go
@@ -2,8 +2,8 @@ package secrets
 
 import (
 	"errors"
-	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/joho/godotenv"
@@ -14,28 +14,32 @@ var envData map[string]string
 func LoadEnv() error {
 	dir, err := os.Getwd()
 
-	pathSplit := strings.Split(dir, "/")
-
-	switch pathSplit[len(pathSplit)-1] {
-	case "2024-S-GROUP6-RW":
-		dir += "/backend"
-	case "backend":
-		// .env file is here
-	case "business":
-		dir += "/.."
-	case "unit_tests":
-		dir += "/../.."
-	default:
-		return errors.New("invalid execution directory")
-	}
-
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(dir)
+	pathSplit := strings.Split(dir, string(filepath.Separator))
 
-	err = godotenv.Load(dir + "/.env")
+	resultPath := []string{}
+
+	for _, str := range pathSplit {
+		resultPath = append(resultPath, str)
+
+		if str == "2024-S-GROUP6-RW" {
+			break
+		}
+	}
+
+	procPath := "/"
+
+	for _, str := range resultPath {
+		procPath = filepath.Join(procPath, str)
+	}
+
+	procPath = filepath.Join(procPath, "backend")
+	procPath = filepath.Join(procPath, ".env")
+
+	err = godotenv.Load(procPath)
 
 	if err != nil {
 		return errors.New(".env file does not exist")
