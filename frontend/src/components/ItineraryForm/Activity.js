3f66b98b9686771d2f91273c9d01b3a7b80edca4
renm10 <43124584+renm10@users.noreply.github.com>
2024-04-28 22:44:16 -0700

    Upgraded UI on Activity pop up

--- frontend/src/components/ItineraryForm/Activity.js
+++ frontend/src/components/ItineraryForm/Activity.js
@@ -1,23 +1,33 @@
 import React, { useState, useContext } from "react";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogContentText from "@mui/material/DialogContentText";
-import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import TodayIcon from "@mui/icons-material/Today";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import CloseIcon from "@mui/icons-material/Close";
+import EditIcon from '@mui/icons-material/Edit';
 import { useTheme } from "@mui/material/styles";
+import {
+  Typography,
+  Button,
+  Dialog,
+  DialogContentText,
+  DialogActions,
+  CardMedia,
+  IconButton,
+  Input,
+  InputBase,
+} from "@mui/material"
+
 import "../../css/Activity.css"
+import AddressAutoFill from "../../components/ItineraryForm/AddressAutoFill";
+
 
 import cityImg from "../../images/itinerary_img/los-angeles-city.jpg";
 
 const Activity = ({ activity }) => {
-  const { id, title, location, date, time, description, photo } = activity; //Get all activity information as a prop
+  const { id, title, start, end, description, location } = activity; //Get all activity information as a prop
 
   const [isDialogOpen, setDialogOpen] = useState(false); //Control pop up window visibility
+  const [isEditMode, setIsEditMode] = useState(false);
   const theme = useTheme();
 
   //Handles open dialogue 
@@ -31,6 +41,11 @@ const Activity = ({ activity }) => {
     setDialogOpen(false);
   };
 
+  //Toggle Edit Mode
+  const toggleEditMode = () => {
+    setIsEditMode((prev) => !prev); //Set it to the opposite mode.
+  }
+
   return (
     <div className="activity">
       <div className="activity-title-btn-container">
@@ -42,9 +57,8 @@ const Activity = ({ activity }) => {
         open={isDialogOpen}
         onClose={handleCloseDialog}
         maxWidth="md"
-        fullWidth //Uses the biggest dialogue size
       >
-        <div className="dialog-content" style={{height: 600}}>
+        <div className="dialog-content" style={{height: 550, width: 650}}>
           <div className="x-button" style={{ position: 'absolute', top: 0, right: 0 }}>
             <IconButton
               aria-label="close"
@@ -58,37 +72,71 @@ const Activity = ({ activity }) => {
           </div>
           <CardMedia
             component="img"
-            height="300"
             image={cityImg}
             alt={title}
             style={{
-              marginBottom: 16,
-              maxHeight: "225px",
+              marginBottom: 5,
+              maxHeight: "160px",
               objectfit: "cover",
             }}
           />
-          <Typography
-            variant="subtitle1"
-            style={{ paddingTop: 4, paddingBottom: 4 }}
-          >
-            <LocationOnIcon style={{ marginBottom: -6 }} /> {location}
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            style={{ paddingTop: 4, paddingBottom: 4 }}
-          >
-            <TodayIcon style={{ marginBottom: -6 }} /> {date}
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            style={{ paddingTop: 4, paddingBottom: 4 }}
-          >
-            <AccessTimeIcon style={{ marginBottom: -6 }} /> {time}
-          </Typography>
-          <DialogContentText style={{ marginTop: 16 }}>
-            <header>Description:</header>
-            {description}
-          </DialogContentText>
+          <div className="editable-content">
+            <div className="title-input-container">
+              <InputBase className={`title-input ${isEditMode ? 'title-input-editmode' : ''}`} defaultValue={title} readOnly={!isEditMode} />
+            </div>
+            <div className="dt-tm-lc-dsc-container">
+              <div className="date-time-container">
+                <div className="date-section-container">
+                  <TodayIcon/>
+                  {isEditMode ? (
+                    <button className="date-btn">
+                      <Typography>{start.toLocaleString('default', {weekday: 'long', month: 'short', day:'2-digit'})}</Typography>
+                    </button>
+                  ) : (
+                    <Typography>{start.toLocaleString('default', {weekday: 'long', month: 'short', day:'2-digit'})}</Typography>
+                  )}
+                </div>
+                <div className="time-section-container">
+                  <AccessTimeIcon/>
+                  {isEditMode ? (
+                    <button className="time-btn">
+                      <Typography>{start.toLocaleString('default', {hour: 'numeric', minute: 'numeric'})} ~ {end.toLocaleString('default', {hour: 'numeric', minute: 'numeric'})}</Typography>
+                    </button>
+                  ) : (
+                    <Typography>{start.toLocaleString('default', {hour: 'numeric', minute: 'numeric'})} ~ {end.toLocaleString('default', {hour: 'numeric', minute: 'numeric'})}</Typography>
+                  )}
+                </div>
+              </div>
+              <div className="location-address-container">
+                <LocationOnIcon style={{ marginBottom: 1 }} />
+                <AddressAutoFill isEditMode={isEditMode}></AddressAutoFill>
+              </div>
+              <Typography className="description-text-activity" >Description</Typography>
+              <InputBase
+                className={`description-inputbase-activity ${isEditMode ? 'description-inputbase-activity-editmode' : ''}`}
+                defaultValue={description}
+                placeholder={!isEditMode ? '' : 'Enter description...'}
+                minRows={5}
+                multiline
+                readOnly={!isEditMode}
+              />
+            </div>
+          </div>
+          <DialogActions>
+            {isEditMode ? (
+              <Button onClick={toggleEditMode}>Cancel</Button>
+            ) : (
+              <IconButton
+                onClick={toggleEditMode}
+                sx={{
+                  color: "black",
+                }}
+              >
+                <EditIcon /> 
+              </IconButton>
+            )}
+            {isEditMode && <Button>Save</Button>}
+          </DialogActions>
         </div>
       </Dialog>
     </div>
